162b8a09fe3d01caf2c87b128eae72028faeab0e
DESARROLLO3\Desarrollo3 <gabrieloros93@gmail.com>
2018-12-27 11:53:06 -0300

    asignacion de reclamos

---
+++ app/assignment/assignment-controller.js
@@ -0,0 +1,146 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('assignmentController', assignmentController);
+
+    assignmentController.$inject = ['$rootScope', '$scope', '$http', '$filter', '$interval', 'statusService', '$state', '$window', '$compile', 'CONSTANTS'];
+
+    function assignmentController($rootScope, $scope, $http, $filter, $interval, statusService, $state, $window, $compile, $CONSTANTS) {
+        var assignmentCntrl = this;
+
+        assignmentCntrl.search = search;
+        assignmentCntrl.form = newAssignment;
+        assignmentCntrl.data = {};
+        $scope.listGroups = [];
+        $scope.listUsers = [];
+        $scope.claimListAssignmentByUser = [];
+        $scope.claimListNotAssignment = [];
+        $scope.userPosition = [];
+        $rootScope.markers = [];
+        $scope.markerAssignmentId = [];
+
+        statusService.getListTheInfoForm({}, function(response) {
+            $scope.listGroups = response.data[1];
+            $scope.listUsers = response.data[2];
+            $scope.listGroups.push({ id: 0, name: "Sin grupo" });
+        }, function(error) {
+            console.log(error);
+        });
+
+        var mapOptions = {
+            zoom: 13,
+            center: new google.maps.LatLng(-32.885, -68.8422),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        $rootScope.mapAssignment = new google.maps.Map(document.getElementById('mapAssignment'), mapOptions);
+
+
+        var drawingManager = new google.maps.drawing.DrawingManager({
+            drawingControl: true,
+            drawingControlOptions: {
+                position: google.maps.ControlPosition.TOP_CENTER,
+                drawingModes: ['polygon']
+            },
+        });
+        drawingManager.setMap($rootScope.mapAssignment);
+        google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+
+            if (event.type == 'polygon') {
+                var polygon = event.overlay;
+
+                angular.forEach($rootScope.markers, function(marker) {
+
+                    if (google.maps.geometry.poly.containsLocation(marker.position, polygon) == true) {
+                        $scope.markerAssignmentId.push(marker.id);
+                    }
+                })
+            }
+        });
+
+
+        function newAssignment() {
+            $http({
+                method: 'POST',
+                url: $CONSTANTS.SERVER_URL + 'assignment',
+                params: { idUser: assignmentCntrl.data.operario, claims: $scope.markerAssignmentId }
+                //params: { identikey: $scope.identikey, data: claimsCntrl.data, positions: $scope.newClaims }
+            }).then(function(response) {
+                if (response.data.result == true) {
+                    alert("Los reclamos fueron asignados correctamente");
+                    $state.go('app');
+                } else {
+                    alert(response.data.data);
+                }
+            });
+        }
+
+        function search() {
+
+            angular.forEach($rootScope.markers, function(marker, key) {
+                marker.setMap(null);
+            });
+            statusService.getListClaimsAssignmentByUser({ userId: assignmentCntrl.data.operario, groupId: assignmentCntrl.data.group }, function(response) {
+                $scope.claimListAssignmentByUser = response.data[1];
+                $scope.userPosition = response.data[0];
+                drawClaimMarkers($scope.claimListAssignmentByUser);
+
+            }, function(error) {
+                console.log(error);
+            });
+            statusService.getListClaimsNotAssignment({ groupId: assignmentCntrl.data.group }, function(response) {
+                $scope.claimListNotAssignment = response.data;
+                drawClaimMarkers($scope.claimListNotAssignment);
+
+            }, function(error) {
+                console.log(error);
+            });
+
+        }
+
+
+        var drawClaimMarkers = function(data) {
+            angular.forEach(data, function(claim, key) {
+                createClaimMarker(claim);
+            });
+        }
+
+        var createClaimMarker = function(claim) {
+            var infoWindow = new google.maps.InfoWindow();
+            if (claim.user_id == 0) {
+                var urlRoute = 'app/mapa/imagen/blue.png'
+            } else {
+                var urlRoute = 'app/mapa/imagen/createClaim.png'
+            }
+            var claimIcon = {
+                url: urlRoute,
+            };
+            var date = new Date(claim.date);
+            var marker = new google.maps.Marker({
+                icon: claimIcon,
+                map: $rootScope.mapAssignment,
+                position: new google.maps.LatLng(claim.latitude, claim.longitude),
+                title: claim.code,
+                content: claim.code,
+                id: claim.id,
+                type: "claim",
+                user_id: claim.user_id
+            });
+
+            var htmlElement = '<div><h4 style="color: #0f0f0f">' + marker.title + '</h4><br><button class="btn btn-primary" ng-click="deleteCLaim(' + marker.id + ')"> Eliminar </button></div>';
+
+            var compiled = $compile(htmlElement)($scope);
+
+            google.maps.event.addListener(marker, 'click', function() {
+                //infoWindow.setContent(compiled[0]);
+                //infoWindow.setContent('<h4 style="color: #0f0f0f">' + marker.title + '</h4><br>' + compiled[0]);
+                infoWindow.open($rootScope.map, marker);
+            });
+            $rootScope.markers.push(marker);
+        }
+
+    }
+
+})();
--- app/services/status-service.js
+++ app/services/status-service.js
@@ -12,7 +12,9 @@
                 'getCountTotal': { url: route + "claimsAmounts/:projectId/:timeCode/:typeCode", method: 'GET', projectId: '@projectId', timeCode: '@projectId', typeCode: '@projectId' },
                 'getClaimAmountsData': { url: route + "claimsAmountsData/:typeCode", method: 'GET', typeCode: '@projectId' },
                 'getListTheInfoForm': { url: route + "dataInfoForm", method: 'GET' },
-                'deleteClaim': { url: route + "deleteClaim/:claimId", claimId: '@claimId', method: 'GET' }
+                'deleteClaim': { url: route + "deleteClaim/:claimId", claimId: '@claimId', method: 'GET' },
+                'getListClaimsAssignmentByUser': { url: route + "listClaimsAssignmentByUser/:userId/:groupId", userId: '@userId', groupId: '@groupId', method: 'GET' },
+                'getListClaimsNotAssignment': { url: route + "listClaimsNotAssignment/:groupId", groupId: '@groupId', method: 'GET' }
             });
             return resource;
         });
